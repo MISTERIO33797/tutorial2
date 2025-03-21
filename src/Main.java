14cc32ccb8c34c9f24ac27f20690bf193772a1e7
MISTERIO33797 <s33797@pjwstk.edu.pl>
2025-03-21 02:23:53 +0100

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,33 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array1 = {3, 3, 3, 2, 2};
-        int[] array2 = {1, 2, 3, 4};
-        int[] array3 = {5, 5, 5, 1};
-        System.out.println(findDominant(array1));
-        System.out.println(findDominant(array2));
-        System.out.println(findDominant(array3));
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {1, 2, 3};
+        System.out.println(Arrays.toString(rotateArray(array1, 2)));
+        System.out.println(Arrays.toString(rotateArray(array2, 3)));
     }
 
-    public static int findDominant(int[] array) {
-        HashMap<Integer, Integer> countMap = new HashMap<>();
+    public static int[] rotateArray(int[] array, int positions) {
         int n = array.length;
-        int threshold = n / 2;
-        for (int num : array) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-            if (countMap.get(num) > threshold) {
-                return num;
-            }
+        if (n == 0) return array;
+
+        positions = positions % n;
+        if (positions == 0) return array;
+        reverse(array, 0, n - 1);
+        reverse(array, 0, positions - 1);
+        reverse(array, positions, n - 1);
+
+        return array;
+    }
+
+    private static void reverse(int[] array, int start, int end) {
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
         }
-        return -1;
     }
 }
